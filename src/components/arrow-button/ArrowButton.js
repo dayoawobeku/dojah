ffcddd62a41b50c55a4af5e471bd0527c4358cb0
Dayo Awobeku <dawobeku@gmail.com>
2021-07-28 21:37:42 +0100

    GitHub page added

--- src/components/arrow-button/ArrowButton.js
+++ src/components/arrow-button/ArrowButton.js
@@ -4,14 +4,12 @@ import "./ArrowButton.css";
 function ArrowButton({ text }) {
   return (
     <div>
-      <a href="#">
-        <button className="btn-default arrow-button">
-          <p>{text}</p>
-          <img
-            src="https://raw.githubusercontent.com/awobekuD/dojah/a185ea8555aaf1b3c7009e12109356ab2cc56463/src/assets/arrow.svg"
-            alt="arrow"
-          />
-        </button>
+      <a href="#" className="btn-default arrow-button">
+        <p>{text}</p>
+        <img
+          src="https://raw.githubusercontent.com/awobekuD/dojah/a185ea8555aaf1b3c7009e12109356ab2cc56463/src/assets/arrow.svg"
+          alt="arrow"
+        />
       </a>
     </div>
   );
